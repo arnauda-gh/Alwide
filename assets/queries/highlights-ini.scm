c8507d8c7509bbcd96e57f7134c72a3fd372d761
arnauda-gh <59512940+arnauda-gh@users.noreply.github.com>
2026-06-11 10:00:16 +0200

    Add ini queries.

---
+++ assets/queries/highlights-ini.scm
@@ -0,0 +1,17 @@
+(section_name
+  (text) @type)
+
+(comment) @comment
+
+[
+  "["
+  "]"
+] @keyword
+
+"=" @operator
+
+(setting
+  (setting_name) @property)
+
+(setting
+  (setting_value) @string)
